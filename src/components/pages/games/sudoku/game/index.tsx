6e006c6d97ec48257e54ccb51214ec70e3229365
Artem Volkov <artyom.volkow@gmail.com>
2022-11-23 23:04:23 +0200

    Sudoku: refactoring of Generator service

--- src/components/pages/games/sudoku/game/index.tsx
+++ src/components/pages/games/sudoku/game/index.tsx
@@ -1,21 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import cloneDeep from 'lodash/cloneDeep';
 
 import GameArea from '@pages/games/sudoku/game/area';
 import GamePanel from '@pages/games/sudoku/game/panel';
 import Modal, { ModalType } from '@pages/games/sudoku/game/modal';
 
-import SudokuGenerator from '@pages/games/sudoku/game/generator';
-
-import { GAME_DATA } from '@pages/games/sudoku/game/data';
+import SudokuGenerator, { Level } from '@pages/games/sudoku/game/generator';
 
 import './style.scss';
-
-export enum Level {
-  Easy = 62,
-  Medium = 53,
-  Hard = 44
-}
 
 export enum Action {
   FILL,
@@ -28,22 +20,23 @@ export enum Action {
   CLOSE_MODAL
 }
 
-const generator = new SudokuGenerator();
-
 const SudokuGameWidget = () => {
+  useEffect(() => {
+    setNewGame(Level.Easy);
+  }, []);
+
+  const [generator] = useState(new SudokuGenerator());
   const [level, setLevel] = useState<Level>(Level.Easy);
   const [hasChange, setHasChange] = useState<boolean>(false);
-  const [blank, setBlank] = useState(GAME_DATA[level].blank);
-  const [filled, setFilled] = useState(GAME_DATA[level].filled);
-  const [blankData, setBlankData] = useState(cloneDeep(GAME_DATA[level].blank));
+  const [blank, setBlank] = useState([]);
+  const [filled, setFilled] = useState([]);
+  const [blankData, setBlankData] = useState([]);
   const [active, setActive] = useState<[block: number, cell: number]>([-1, -1]);
   const [hints, setHints] = useState<number>(5);
   const [history, setHistory] = useState<Array<{ block: number, cell: number, value: number }>>([]);
   const [modal, setModal] = useState<{ open: boolean, type: ModalType, onConfirm?: () => void}>({
     open: false, type: null
   });
-
-  console.log(generator.generate(level));
 
   const onAction = (action, data = null) => {
     switch (action) {
@@ -101,11 +94,13 @@ const SudokuGameWidget = () => {
   };
 
   const setNewGame = (level) => {
+    const sudoku = generator.generate(level);
+
     setHasChange(false);
     setLevel(level);
-    setBlank(GAME_DATA[level].blank);
-    setBlankData(cloneDeep(GAME_DATA[level].blank));
-    setFilled(GAME_DATA[level].filled);
+    setBlank(sudoku.blank);
+    setBlankData(cloneDeep(sudoku.blank));
+    setFilled(sudoku.filled);
     setHistory([]);
     setHints(5);
   };
@@ -135,42 +130,18 @@ const SudokuGameWidget = () => {
     if (hints <= 0) {
       return;
     }
+    const data = SudokuGenerator.setHint(blankData, filled);
 
-    const blocks = [];
-    const emptyBlocs = [];
-    const newData = [...blankData];
-
-    blankData.forEach((row, index) => {
-      const hasEmpty = row.some((item) => !item);
-
-      if (!hasEmpty) {
-        blocks[index] = null;
-        return;
-      }
-      emptyBlocs.push(index);
-      blocks[index] = row.reduce((prev, item, index) => {
-        if (!item) {
-          prev.push(index);
-        }
-        return prev;
-      }, []);
-    });
-
-    if (!emptyBlocs.length) {
+    if (!data) {
       return;
     }
 
     if (!hasChange) {
       setHasChange(true);
     }
-
-    const blockIndex = emptyBlocs[Math.round(Math.random() * (emptyBlocs.length-1))];
-    const cellIndex = blocks[blockIndex][Math.round(Math.random() * (blocks[blockIndex].length-1))];
-
-    newData[blockIndex][cellIndex] = filled[blockIndex][cellIndex];
     setHints(hints-1);
-    setBlankData(newData);
-    checkIsFinish(newData);
+    setBlankData(data);
+    checkIsFinish(data);
   }
 
   const undo = () => {
@@ -195,14 +166,14 @@ const SudokuGameWidget = () => {
   };
 
   const checkIsFinish = (data) => {
-    const isFinish = !data.some((itemRow, row) => itemRow.some((item, cell) => filled[row][cell] !== item));
-
-    if (isFinish) {
-      setModal({
-        open: true,
-        type: ModalType.FINISH
-      });
+    if (!SudokuGenerator.isFinish(data, filled)) {
+     return;
     }
+    setModal({
+      open: true,
+      type: ModalType.FINISH,
+      onConfirm: () => setNewGame(level),
+    });
   };
 
   return (
