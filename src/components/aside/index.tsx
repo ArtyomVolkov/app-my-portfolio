6062b154705d830c157d402a551c7aa63b8b6783
artemvolkov <artyom.volkow@gmail.com>
2022-11-24 20:29:11 +0200

    profile page: add responsive layout

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import IconButton from '@mui/material/IconButton';
+import ArrowLeft from '@mui/icons-material/ArrowBackRounded';
+import ArrowRight from '@mui/icons-material/ArrowForwardRounded';
+
 import Avatar from '@components/aside/avatar';
 import Navigation from '@components/aside/navigation';
 
+import appActions from '@store/actions/app';
 import Image from '@assets/images/a.volkov.png';
 
 import './style.scss';
@@ -15,6 +20,9 @@ const SideBar = () => {
         subtitle="SR. FrontEnd engineer"
         image={Image}
       />
+      <IconButton className="screen-resize" onClick={appActions.toggleFullWidth}>
+        <ArrowLeft />
+      </IconButton>
       <Navigation />
     </aside>
   );
