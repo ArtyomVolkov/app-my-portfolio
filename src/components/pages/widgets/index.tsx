41dd671525a4a8000a317b6b9e6a220b8036178f
Artem Volkov <artyom.volkow@gmail.com>
2023-02-17 22:32:41 +0200

    scss migration, (widgets page)

--- src/components/pages/widgets/index.tsx
+++ src/components/pages/widgets/index.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 import Main from '@components/main';
 import NavButton from '@shared/components/buttons/navigation';
 
-import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
+// import HistoryRoundedIcon from '@mui/icons-material/HistoryRounded';
 import CollectionsIcon from '@mui/icons-material/Collections';
 
-import './style.scss';
+import styles from './style.module.scss';
 
 const WidgetsPage = () => {
   return (
-    <Main className="widgets-page">
-      <NavButton
-        path="/widgets/history"
-        title="History"
-        icon={<HistoryRoundedIcon className="icon"/>}
-      />
+    <Main className={styles.widgetsPage}>
+      {/*<NavButton*/}
+      {/*  path="/widgets/history"*/}
+      {/*  title="History"*/}
+      {/*  icon={<HistoryRoundedIcon className="icon"/>}*/}
+      {/*/>*/}
       <NavButton
         path="/widgets/gallery"
         title="Gallery"
