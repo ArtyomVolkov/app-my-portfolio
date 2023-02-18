8ba335fc26e124b61f0b0fe9d1d585ecdf322a7e
Artem Volkov <artyom.volkow@gmail.com>
2023-02-18 18:20:51 +0200

    update nature-gallery request query

--- src/api/pexels.ts
+++ src/api/pexels.ts
@@ -2,7 +2,7 @@ const API = 'https://api.pexels.com/v1/search'
 const API_KEY = 'BmbfNRvHgMVEzF9ayEeSYNOefT6v2RwnQK6TX9Jpd2AgFg79fYVJ5ROn';
 
 export const getImages = async (page: number, perPage: number) => {
-  return await fetch(`${API}?query=nature&page=${page}&per_page=${perPage}`, {
+  return await fetch(`${API}?query=nature&orientation=landscape&page=${page}&per_page=${perPage}`, {
     headers: {
       Authorization: API_KEY
     }
