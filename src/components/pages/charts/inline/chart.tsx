d76a7ca817f5a0a81e0bc81d877382fae3dd36b1
Artem Volkov <artyom.volkow@gmail.com>
2024-02-21 20:05:50 +0200

    implementation of chart settings

--- src/components/pages/charts/inline/chart.tsx
+++ src/components/pages/charts/inline/chart.tsx
@@ -1,6 +1,15 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
+
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import LineAxisRoundedIcon from '@mui/icons-material/LineAxisRounded';
+import RefreshRoundedIcon from '@mui/icons-material/RefreshRounded';
 
 import styles from './style.module.scss';
+import Slider from '@mui/material/Slider';
 
 const pointsCount = 10;
 const smoothing = 0.15;
@@ -14,12 +23,12 @@ const offsets = {
 const stepX = 40; // step by x axis
 const stepY = 50; // depends on point values
 
-const chartPoints = Array(pointsCount).fill(0).map(() => Math.round(Math.random()*maxValue));
-
-const pointsData = chartPoints.map((item, index) => [index*stepX+offsets.left, maxValue-item+offsets.top]);
-
 const InlineChart = () => {
-  const [settings] = useState({
+  const [chartPoints, setChartPoints] = useState(Array(pointsCount).fill(0).map(() => Math.round(Math.random()*maxValue)));
+  const [settings, setSettings] = useState({
+    type: 'inline',
+    showDots: true,
+    showGradient: false,
     zoom: 1,
     viewBox: {
       width: 360 + offsets.left + offsets.right,
@@ -30,6 +39,14 @@ const InlineChart = () => {
       height: 200
     }
   });
+
+  const pointsData = useMemo(() => {
+    return  chartPoints.map((item, index) => [index*stepX+offsets.left, maxValue-item+offsets.top]);
+  }, [chartPoints]);
+
+  const onRefreshPoints = () => {
+    setChartPoints(Array(pointsCount).fill(0).map(() => Math.round(Math.random()*maxValue)));
+  };
 
   const inlinePath = (point) => `L ${point[0]} ${point[1]}`;
 
@@ -102,6 +119,9 @@ const InlineChart = () => {
   };
 
   const renderCirclePoints = () => {
+    if (!settings.showDots) {
+      return null;
+    }
     return pointsData.map(([x, y], index) => (
       <circle cx={x} cy={y} r={5} key={index} className={styles.circlePoint} />
     ));
@@ -109,33 +129,81 @@ const InlineChart = () => {
 
   return (
     <section className={styles.chartWidget}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={`${settings.viewBox.width * settings.zoom}px`}
-        height={`${settings.viewBox.height * settings.zoom}px`}
-        viewBox={`0 0 ${settings.viewBox.width} ${settings.viewBox.height}`}
-      >
-        {/*<rect width={settings.viewBox.width} height={settings.viewBox.height} fill="none" stroke="black" strokeWidth={1} />*/}
-        <g className={styles.xAxisLines}>{renderXAxis()}</g>
-        <g className={styles.yAxisLines}>{renderYAxis()}</g>
-        <g className={styles.chartBox}>
-          {/*<path*/}
-          {/*  d={getSvgPath(pointsData, inlinePath)}*/}
-          {/*  stroke="black"*/}
-          {/*  strokeWidth="2"*/}
-          {/*  fill="none"*/}
-          {/*/>*/}
-          <path
-            d={getSvgPath(pointsData, bezierPath)}
-            className={styles.strokeLine}
-            strokeWidth="2"
-            fill="none"
+      <div className={styles.chartBox}>
+        <span className={styles.title}>Some Data</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={`${settings.viewBox.width * settings.zoom}px`}
+          height={`${settings.viewBox.height * settings.zoom}px`}
+          viewBox={`0 0 ${settings.viewBox.width} ${settings.viewBox.height}`}
+        >
+          {/*<rect width={settings.viewBox.width} height={settings.viewBox.height} fill="none" stroke="black" strokeWidth={1} />*/}
+          <g className={styles.xAxisLines}>{renderXAxis()}</g>
+          <g className={styles.yAxisLines}>{renderYAxis()}</g>
+          <g className={styles.chartBox}>
+            <path
+              d={getSvgPath(pointsData,settings.type === 'curve' ? bezierPath : inlinePath)}
+              className={styles.strokeLine}
+              strokeWidth="2"
+              fill="none"
+            />
+            {
+              renderCirclePoints()
+            }
+          </g>
+        </svg>
+      </div>
+      <div className={styles.settings}>
+        <div className={styles.options}>
+          <div className={styles.option}>
+            <Button
+              variant="outlined"
+              fullWidth
+              startIcon={<RefreshRoundedIcon />}
+              onClick={onRefreshPoints}
+            >
+              Refresh
+            </Button>
+          </div>
+          <div className={styles.option}>
+            <ButtonGroup variant="outlined" fullWidth>
+              <Button
+                variant={settings.type === 'inline' ? 'contained' : 'outlined'}
+                onClick={() => setSettings({ ...settings, type: 'inline' })}
+              >
+                <TimelineIcon />
+              </Button>
+              <Button
+                variant={settings.type === 'curve' ? 'contained' : 'outlined'}
+                onClick={() => setSettings({ ...settings, type: 'curve' })}
+              >
+                <LineAxisRoundedIcon />
+              </Button>
+            </ButtonGroup>
+          </div>
+          <span className={styles.optionLabel}>Points</span>
+          <div className={styles.option}>
+            <Slider
+              defaultValue={10}
+              aria-label="Small"
+              marks
+              min={3}
+              max={10}
+              valueLabelDisplay="auto"
+            />
+          </div>
+          <FormControlLabel
+            label="Show dots"
+            control={<Checkbox checked={settings.showDots} />}
+            onChange={(e: any) => setSettings({...settings, showDots: e.target.checked })}
           />
-          {
-            renderCirclePoints()
-          }
-        </g>
-      </svg>
+          <FormControlLabel
+            label="Show gradient"
+            control={<Checkbox checked={settings.showGradient} />}
+            onChange={(e: any) => setSettings({...settings, showGradient: e.target.checked })}
+          />
+        </div>
+      </div>
     </section>
   );
 };
