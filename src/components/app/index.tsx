4717e963eedbc330f0b728c69085f214f2763b92
Artem Volkov <artyom.volkow@gmail.com>
2025-12-04 00:11:38 +0200

    fix issues

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,18 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { HashRouter } from "react-router-dom";
 import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 
 import AppContent from "@components/app/app-content";
-import MFEService from "@services/mfe";
 
 import THEME from "@assets/theme/main";
 
 const App = () => {
-  useEffect(() => {
-    MFEService.checkMFEAppsAuth();
-  }, []);
-
   return (
     <HashRouter>
       <StyledEngineProvider injectFirst>
